73e1556960139bf0e8c00e689668b3a88df2322c
outcastgeek <outcastgeek+git@gmail.com>
2019-01-13 19:46:17 -0600

    Part 3

--- R/ANLY530_Laboratory2_Chunks.R
+++ R/ANLY530_Laboratory2_Chunks.R
@@ -135,3 +135,77 @@ table(filteredTestPred, filteredTest$Creditability)
 
 (conf_nat <- table(filteredTestPred, filteredTest$Creditability))
 (Accuracy <- sum(diag(conf_nat))/sum(conf_nat)*100)
+
+## @knitr part3Step1
+
+# Data Preperation
+newsShort <- data.frame(
+  onlineNewsPopularity$n_tokens_title,
+  onlineNewsPopularity$n_tokens_content,
+  onlineNewsPopularity$n_unique_tokens,
+  onlineNewsPopularity$n_non_stop_words,
+  onlineNewsPopularity$num_hrefs,
+  onlineNewsPopularity$num_imgs,
+  onlineNewsPopularity$num_videos,
+  onlineNewsPopularity$average_token_length,
+  onlineNewsPopularity$num_keywords,
+  onlineNewsPopularity$kw_max_max,
+  onlineNewsPopularity$global_sentiment_polarity,
+  onlineNewsPopularity$avg_positive_polarity,
+  onlineNewsPopularity$title_subjectivity,
+  onlineNewsPopularity$title_sentiment_polarity,
+  onlineNewsPopularity$abs_title_subjectivity,
+  onlineNewsPopularity$abs_title_sentiment_polarity,
+  onlineNewsPopularity$shares
+  )
+
+colnames(newsShort) <- c(
+  "n_tokens_title",
+  "n_tokens_content",
+  "n_unique_tokens",
+  "n_non_stop_words",
+  "num_hrefs",
+  "num_imgs",
+  "num_videos",
+  "average_token_length",
+  "num_keywords",
+  "kw_max_max",
+  "global_sentiment_polarity",
+  "avg_positive_polarity",
+  "title_subjectivity",
+  "title_sentiment_polarity",
+  "abs_title_subjectivity",
+  "abs_title_sentiment_polarity",
+  "shares"
+  )
+
+# Data Preperation
+newsShort$popular = rep('na', nrow(newsShort))
+for(i in 1:39644) {
+  if(newsShort$shares[i] >= 1400) {
+    newsShort$popular[i] = "yes"} 
+  else {newsShort$popular[i] = "no"}
+}
+newsShort$shares = newsShort$popular
+newsShort$shares <- as.factor(newsShort$shares)
+
+# Data Preperation
+news_rand <- newsShort[order(runif(10000)), ]
+
+# Split the data into training and test datasets
+news_train <- news_rand[1:9000, ]
+news_test <- news_rand[9001:10000, ]
+
+
+## @knitr part3Step2
+
+nb_model <- naive_bayes(shares ~ ., data=news_train)
+nb_model
+
+## @knitr part3Step3
+
+news_Pred <- predict(nb_model, newdata = news_test)
+(conf_nat <- table(news_Pred, news_test$shares))
+
+(Accuracy <- sum(diag(conf_nat))/sum(conf_nat)*100)
+
